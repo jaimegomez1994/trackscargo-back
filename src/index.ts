98f9c7191f9c5e9eae07e3c1bd5f160007c9d9be
Your Name <your.email@example.com>
2025-07-26 16:01:44 -0600

    Added cors; Added small shipments endpoint

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,18 @@
-import express from 'express';
+import express from "express";
+import cors from "cors";
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+app.use(cors());
 app.use(express.json());
 
-app.get('/', (req, res) => {
-  res.json({ message: 'Backend API is running!' });
+app.get("/", (req, res) => {
+  res.json({ message: "Backend API is running!" });
+});
+
+app.get("/shipments", (req, res) => {
+  res.json({ shipments: ["XX1234567890", "XX1234567891"] });
 });
 
 app.listen(PORT, () => {
